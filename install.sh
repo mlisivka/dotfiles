2e3bcfd4a77d98d07ae6d6d62f7cb9bc578859bf
Mykola Lisivka <12681237+mlisivka@users.noreply.github.com>
2025-07-05 15:27:46 +0300

    Update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip3 install pynvim
+pip install pynvim
 brew install fzf
 brew install bat
 brew install zsh
@@ -9,7 +9,7 @@ brew install neovim
 brew install direnv
 brew install pyenv
 brew install watchman
-brew insatll libpq # psql/pg_dump
+brew install libpq # psql/pg_dump
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 if [ ! -d ~/.config/base16-shell ]; then
   git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
@@ -17,14 +17,11 @@ fi
 if [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
-if [ ! -d $HOME/.rbenv/plugins/rbenv-gemset ]; then
-  git clone git://github.com/jf/rbenv-gemset.git $HOME/.rbenv/plugins/rbenv-gemset
-fi
 if [ ! -d "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ]; then
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v1.40.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 nvm install node
 nvm use node
 npm install -g yarn
